43a00dcf6a838444cc4443e47046c7921080dcde
LeeDarmon <leemickael.darmon@outlook.fr>
2021-03-09 14:03:10 +0100

    update models

--- application/models/Parking_model.php
+++ application/models/Parking_model.php
@@ -26,6 +26,7 @@ Class Parking_model extends CI_Model {
 
     public function update($id,$data)
     {
+        $this->db->set($data); 
         $this->db->where('id', $id);
         return $this->db->update('Parking', $data);
     }
--- application/models/Rent_model.php
+++ application/models/Rent_model.php
@@ -26,6 +26,7 @@ Class Rent_model extends CI_Model {
 
     public function update($id,$data)
     {
+        $this->db->set($data); 
         $this->db->where('id', $id);
         return $this->db->update('Rent', $data);
     }
