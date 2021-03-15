f224ede20b787665cf6a4a57247c698a92aab0a1
LeeDarmon <leemickael.darmon@outlook.fr>
2021-03-15 16:15:17 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,24 +10,6 @@ $(document).ready(function () {
 	});
 });
 
-var today = new Date().toISOString().split("T")[0];
-console.log("script connecté");
-console.log(today);
-
-$("#end_date").change(function () {
-	var date1 = new Date($("#start_date").val());
-	var date2 = new Date($("#end_date").val());
-	var Diff_temps = date2.getTime() - date1.getTime();
-	var Diff_jours = Diff_temps / (1000 * 3600 * 24);
-	$("#final_days").html(Diff_jours);
-
-	var price = $("#forfait").html();
-	console.log(price);
-	var final_price = price * Diff_jours;
-	$("#final_price").html(final_price);
-	$("#cost").val(final_price);
-});
-
     var today = new Date().toISOString().split('T')[0];
     console.log("script connecté");
     console.log(today);
